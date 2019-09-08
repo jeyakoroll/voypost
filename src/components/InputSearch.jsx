ce1e3589c756581be0e7b9a47d4e005e5795c4dd
Jenya Korolenko <jeya-koroll@ukr.net>
2019-09-08 20:23:07 +0300

    add some logo with link to git on this page

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -3,18 +3,13 @@ import { Input } from 'antd';
 
 const Search = Input.Search;
 
-class InputSearch extends React.Component {
-
-  render () {
-    const { setInputValue } = this.props
-
-    return <Search
-      className="input-search"
-      placeholder="input search text"
-      onChange = { ({target: { value }}) => setInputValue(value) }
-      style={{ width: 200 }}
-    />
-  }
-}
+const InputSearch = ({ setInputValue }) => (
+ <Search
+    className="input-search"
+    placeholder="input search text"
+    onChange = { ({target: { value }}) => setInputValue(value) }
+    style={{ width: 200 }}
+  />
+)
 
 export default InputSearch;
