957e67f45ab5bb46891b5bd6f63aeff575999a27
Jenya Korolenko <jeya-koroll@ukr.net>
2019-09-14 23:30:35 +0300

    push with build

---
+++ build/precache-manifest.1e233c5706e2f3fd269cae59ec5497a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96168d8fc2e90e165c2559c74a7aabd5",
+    "url": "/voypost/index.html"
+  },
+  {
+    "revision": "a1a59380d645a7dd6c18",
+    "url": "/voypost/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "bc4c840ec21d018d7b71",
+    "url": "/voypost/static/css/main.9db02ef5.chunk.css"
+  },
+  {
+    "revision": "a1a59380d645a7dd6c18",
+    "url": "/voypost/static/js/2.b2788c2c.chunk.js"
+  },
+  {
+    "revision": "bc4c840ec21d018d7b71",
+    "url": "/voypost/static/js/main.b6e1ba0a.chunk.js"
+  },
+  {
+    "revision": "467093b850f5ff6b6faa",
+    "url": "/voypost/static/js/runtime~main.01fdbd1c.js"
+  }
+]);
